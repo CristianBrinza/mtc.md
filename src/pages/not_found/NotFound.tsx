a8352354a03a8a107de63d30801d217bff22eba4
Cristian Brinza <cristian.brinza@isa.utm.md>
2025-06-20 14:13:33 +0300

    Revamps promo page and adds 404 handler
    Updates promotional page translations for improved clarity.
    Adds a new 404 Not Found page with styling and navigation.
    Refactors CSS for consistency in hover transitions.
    
    Enhances user experience by making error handling more user-friendly and
    improving promotional content accessibility.
    
    Relates to UX enhancements.

--- src/pages/not_found/NotFound.tsx
+++ src/pages/not_found/NotFound.tsx
@@ -1,4 +1,48 @@
 //import styles from './NotFound.module.css';
+import { useTranslation } from 'react-i18next';
+import Navbar from '../../components/navbar/Navbar.tsx';
+import Chat from '../../components/chat/Chat.tsx';
+import Feedback from '../../components/feedback/Feedback.tsx';
+import Breadcrumb from '../../components/Breadcrumb/Breadcrumb.tsx';
+import Footer from '../../components/footer/Footer.tsx';
+import styles from './NotFound.module.css';
+import Button from '../../components/Button.tsx';
+
 export default function NotFound() {
-  return <>404 | Not Found</>;
+  const { t } = useTranslation();
+  const breadcrumbItems = [{ label: t('notfound.breadcrumb.title') }];
+
+  return (
+    <>
+      <Navbar />
+      <Chat />
+      <Feedback />
+      <Breadcrumb items={breadcrumbItems} />
+
+      <div className={styles.notfound}>
+        <div className={styles.notfound_title}>
+          {/*404  - Not Found*/}
+          404.
+        </div>
+
+        <div className={`title_3 ${styles.notfound_subtitle}`}>
+          {t('notfound.subtitle')}
+        </div>
+        <div className={styles.notfound_text}>{t('notfound.text')}</div>
+
+        <Button
+          to="/"
+          color={'#fff'}
+          bgcolor={'var(--theme_primary_color_blue_4)'}
+          border={'var(--theme_primary_color_blue_4)'}
+          hover_border={'var(--theme_primary_color_blue_2)'}
+          hover_bgcolor={'var(--theme_primary_color_blue_2)'}
+          icon={'arrow_right'}
+        >
+          {t('notfound.btn')}
+        </Button>
+      </div>
+      <Footer disclaimer={true} />
+    </>
+  );
 }
