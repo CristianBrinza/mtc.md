cec0ef55f31f139966a03c5a00551538a7e0af3c
Cristian Brinza <cristian.brinza@isa.utm.md>
2025-07-17 15:23:37 +0300

    Removes unused React Router Link import
    Cleans up the ConsentBanner component by eliminating the
    unused import of Link from react-router-dom, streamlining
    the codebase and improving maintainability.

--- src/components/consent_banner/ConsentBanner.tsx
+++ src/components/consent_banner/ConsentBanner.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './ConsentBanner.css';
 import Button from '../Button.tsx';
-import { Link } from 'react-router-dom';
 
 declare global {
   interface Window {
