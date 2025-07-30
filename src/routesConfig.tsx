9d8d97ec24029521bd972ba1ea6dc1e9ccdf66ce
Cristian Brinza <cristian.brinza@isa.utm.md>
2025-07-30 12:37:36 +0300

    Removes test page routes
    Eliminates obsolete routes for test pages in preparation
    for production deployment and to simplify the navigation
    configuration.
    
    No related issue was specified.

--- src/routesConfig.tsx
+++ src/routesConfig.tsx
@@ -67,14 +67,6 @@ export const routesConfig: RouteConfig[] = [
   //   element: React.createElement(TestPage),
   // },
   {
-    path: '/:lang/test_2312',
-    element: React.createElement(FormTestPage),
-  },
-  {
-    path: '/:lang/test_2313',
-    element: React.createElement(FormTestPage),
-  },
-  {
     path: '/:lang/magazine',
     element: React.createElement(Magazine),
   },
